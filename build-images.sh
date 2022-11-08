c6fde682c967de5b28aa6e41a8030703ac630b6e
Davide Principi <davide.principi@nethesis.it>
2022-11-08 15:11:34 +0100

    Fix set-output build warning
    The `set-output` command is deprecated and will be disabled soon. Please
    upgrade to using Environment Files. For more information see:
    https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/

--- build-images.sh
+++ build-images.sh
@@ -204,9 +204,10 @@ images+=("${repobase}/${reponame}")
 #
 # Setup CI when pushing to Github. 
 # Warning! docker::// protocol expects lowercase letters (,,)
-if [[ -n "${CI}" ]]; then
+if [[ -n "${GITHUB_OUTPUT}" ]]; then
     # Set output value for Github Actions
-    printf "::set-output name=images::%s\n" "${images[*],,}"
+    printf "images=%s\n" "${images[*],,}" >> "${GITHUB_OUTPUT}"
+    printf " - %s:${IMAGETAG:-latest}\n" "${images[@],,}" >> $GITHUB_STEP_SUMMARY
 else
     # Just print info for manual push
     printf "Publish the images with:\n\n"
