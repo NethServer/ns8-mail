42a7e5e9b099dd75c4c98cabc0edcf4b51ce81af
Andrea Leardini <andre8244@gmail.com>
2023-04-03 14:36:10 +0200

    ui: fix user mailboxes sorting by quota

--- ui/src/components/mailboxes/UserMailboxes.vue
+++ ui/src/components/mailboxes/UserMailboxes.vue
@@ -12,6 +12,7 @@
             :columns="i18nTableColumns"
             :rawColumns="tableColumns"
             :sortable="true"
+            :customSortTable="sortUserMailboxes"
             :pageSizes="[10, 25, 50, 100]"
             :overflow-menu="true"
             isSearchable
@@ -373,6 +374,24 @@ export default {
     setMailboxEnabledCompleted() {
       this.loading.setMailboxEnabled = false;
       this.listUserMailboxes();
+    },
+    sortUserMailboxes(sortProperty) {
+      if (sortProperty != "quota") {
+        // default sort
+        return this.sortByProperty(sortProperty);
+      }
+
+      // sort by quota
+
+      return function (a, b) {
+        if (a.quota.value < b.quota.value) {
+          return -1;
+        }
+        if (a.quota.value > b.quota.value) {
+          return 1;
+        }
+        return 0;
+      };
     },
   },
 };
