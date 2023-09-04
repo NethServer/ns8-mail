185d0af2dd4463bb23722e0de637654356aed92b
Stephane de Labrusse <stephdl@de-labrusse.fr>
2023-09-04 11:22:57 +0200

    Build and clean the flatcurv installation

--- build-images.sh
+++ build-images.sh
@@ -74,6 +74,22 @@ apk add --no-cache rspamd-client
     mv -v .post-install /usr/local/bin/dovecot-post-install
     rm -rvf "${tmpdir}"
 )
+(
+    apk add build-base git autoconf automake libtool dovecot-dev  xapian-core-dev  icu-dev
+    mkdir /tmp/build
+    cd /tmp/build
+    git init
+    git config --global --add safe.directory /tmp/build
+    git clone https://github.com/slusarz/dovecot-fts-flatcurve.git
+    cd dovecot-fts-flatcurve/
+    ash autogen.sh
+    ./configure --with-dovecot=/usr/lib/dovecot/
+    make
+    make install
+    # clean what we installed
+    rm -rf /tmp/build
+    apk del build-base git autoconf automake libtool dovecot-dev  xapian-core-dev  icu-dev
+)
 mkdir -p /var/lib/dovecot/dict/uquota
 mkdir -p /var/lib/umail
 sed -i 's/^!/#!/' /etc/dovecot/conf.d/10-auth.conf
