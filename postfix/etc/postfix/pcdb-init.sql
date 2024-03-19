31d4b0fb0d5a42c1fdb122e0cba6dd451791b965
Davide Principi <davide.principi@nethesis.it>
2024-03-19 18:37:49 +0100

    postfix. Upgrade SQLite database schema
    Apply SQL scripts to upgrade the SQLite database schema.

--- postfix/etc/postfix/pcdb-init.sql
+++ postfix/etc/postfix/pcdb-init.sql
@@ -126,17 +126,5 @@ CREATE TABLE mynetworks (
     PRIMARY KEY ("network")
 );
 
--- relayrules -- relay host configuration matching a sender or a
--- destination pattern. Stored attributes are host, port, auth
--- credentials, rule description and the "enabled" flag
-CREATE TABLE relayrules (
-    sender TEXT,
-    dest TEXT,
-    host TEXT NOT NULL,
-    port INT DEFAULT 25,
-    username TEXT,
-    "password" TEXT,
-    rdesc TEXT,
-    "enabled" INT DEFAULT 1,
-    UNIQUE (sender, dest, host, port)
-);
+-- READ (INCLUDE) schema updates
+.read /etc/postfix/pcdb-relayrules.sql
