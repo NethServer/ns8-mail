fcd13809a723ce37bed8919c4219341cab996dff
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-05-30 16:27:44 +0200

    Update rspamd Alpine version to 3.20.0

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,7 @@
 set -e
 
 alpine_version=3.17.2
+rspamd_alpine_version=3.20.0
 
 # Prepare variables for later use
 images=()
@@ -153,7 +154,7 @@ images+=("${repobase}/${reponame}")
 # Rspamd additional image
 #
 reponame="mail-rspamd"
-container=$(buildah from docker.io/library/alpine:${alpine_version})
+container=$(buildah from docker.io/library/alpine:${rspamd_alpine_version})
 buildah run "${container}" /bin/sh <<EOF
 set -e
 # Software installation order is important to preserve uid and gid allocation:
