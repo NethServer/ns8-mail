779a10352d407d8c03808c8a4f1c204211aff10f
Davide Principi <davide.principi@nethesis.it>
2024-06-03 09:00:10 +0200

    Update test-module.sh

--- test-module.sh
+++ test-module.sh
@@ -21,7 +21,7 @@ podman run -i \
     --network=host \
     -v .:/home/pwuser/ns8-module:z \
     --volume=site-packages:/home/pwuser/.local/lib/python3.8/site-packages:Z \
-    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:v18.5.1 \
+    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:18.5.1 \
     bash -l -s <<EOF
 set -e
 echo "$ssh_key" > /home/pwuser/ns8-key
