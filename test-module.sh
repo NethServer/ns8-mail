e0197aa716223332a25360d3f65ba06acbe9d1ec
Davide Principi <davide.principi@nethesis.it>
2025-01-13 09:36:21 +0100

    Use base ns8 Renovate config

--- test-module.sh
+++ test-module.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+#
+# Copyright (C) 2024 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+#
+# Hint: access the test logs on HTTP port 8000 with this command:
+#
+#     python -mhttp.server -d tests/outputs/ 8000 &
+#
+
 set -e
 
-LEADER_NODE=$1
-IMAGE_URL=$2
-shift 2
 SSH_KEYFILE=${SSH_KEYFILE:-$HOME/.ssh/id_rsa}
+
+LEADER_NODE="${1:?missing LEADER_NODE argument}"
+IMAGE_URL="${2:?missing IMAGE_URL argument}"
+shift 2
 
 ssh_key="$(< $SSH_KEYFILE)"
 
@@ -19,8 +31,8 @@ cleanup() {
 trap cleanup EXIT
 podman run -i \
     --network=host \
-    -v .:/home/pwuser/ns8-module:z \
-    --volume=site-packages:/home/pwuser/.local/lib/python3.8/site-packages:Z \
+    --volume=.:/home/pwuser/ns8-module:z \
+    --volume=site-packages:/home/pwuser/.local/lib/python3.12/site-packages:z \
     --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:19.1.2 \
     bash -l -s <<EOF
 set -e
@@ -31,7 +43,7 @@ cd /home/pwuser/ns8-module
 exec robot -v NODE_ADDR:${LEADER_NODE} \
     -v IMAGE_URL:${IMAGE_URL} \
     -v SSH_KEYFILE:/home/pwuser/ns8-key \
-    --name mail \
+    --name "$(basename $PWD)" \
     --skiponfailure unstable \
     -d ~/outputs ${@} /home/pwuser/ns8-module/tests/
 EOF
