eb1a79582ea8ca54b9ef73caba8f90be184c9389
Andrea Leardini <andre8244@gmail.com>
2026-04-13 11:21:27 +0200

    test screenshots comment

--- ui/src/views/About.vue
+++ ui/src/views/About.vue
@@ -3,7 +3,7 @@
   SPDX-License-Identifier: GPL-3.0-or-later
 -->
 <template>
-  <!-- test change 1 //// -->
+  <!-- test change 2 //// -->
   <cv-grid fullWidth>
     <cv-row>
       <cv-column class="page-title">
